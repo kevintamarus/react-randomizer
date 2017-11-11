d3226a31452849a33f8b7a7f07e602eb429d8b43
Kevin Tamarus Tang <kevintamarus@gmail.com>
2017-11-11 01:10:17 -0800

    temp removed arguments cases, readme updated

---
+++ README.md
@@ -0,0 +1,15 @@
+## React Randomizer
+
+https://www.npmjs.com/package/react-randomizer
+
+`npm install --save react-randomizer`
+
+example use case:
+
+`import Randomizer from 'react-randomizer'`
+
+
+`Randomizer.randomNumber(num1, num2)`: generates a number between `num1` and `num2`
+
+
+`Randomizer.randomizeArray(arr)`: randomizes/shuffles array
--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 module.exports = {
-  generateRandomNumber: (num1, num2) => {
-    if(arguments.length === 0) {
-      return Math.floor(Math.random() * 100) + 1;
-    }
-    if(arguments.length === 1) {
-      let n = 0;
-      if(num1 > 0) {n = 1}
-      return Math.floor(Math.random() * num1) + n;
-    }
-    if(n1 > n2) {
-      const n1 = num2;
-      const n2 = num1;
-    } else {
-      const n1 = num1;
-      const n2 = num2;
+  randomNumber: (num1, num2) => {
+    let n1 = num1;
+    let n2 = num2;
+    if(num1 > num2) {
+      n1 = num2;
+      n2 = num1;
     }
     return Math.floor(Math.random() * (n2 - n1 + 1) + n1);
   },
